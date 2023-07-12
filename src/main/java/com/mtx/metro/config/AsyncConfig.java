bf0b579afd1260a78adafce87ee7f68fd9c18d04
IamStarlight <108684310+IamStarlight@users.noreply.github.com>
2023-07-12 09:02:08 +0800

    修改了站点映射

--- src/main/java/com/mtx/metro/config/AsyncConfig.java
+++ src/main/java/com/mtx/metro/config/AsyncConfig.java
@@ -12,7 +12,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 public class AsyncConfig {
 
-    @Bean("doSomethingExecutor")
+    @Bean("MyThreadPoolExecutor")
     public Executor doSomethingExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         // 核心线程数：线程池创建时候初始化的线程数
@@ -24,7 +24,7 @@ public class AsyncConfig {
         // 允许线程的空闲时间60秒：当超过了核心线程之外的线程在空闲时间到达之后会被销毁
         executor.setKeepAliveSeconds(60);
         // 线程池名的前缀：设置好了之后可以方便我们定位处理任务所在的线程池
-        executor.setThreadNamePrefix("do-something-");
+        executor.setThreadNamePrefix("MyThreadPool");
         // 缓冲队列满了之后的拒绝策略：由调用线程处理（一般是主线程）
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
         executor.initialize();
